edb1d7fe6ad0ea40f490b9edd39c3ce84334cd2d
Przemyslaw Dabrowski <codefactorypdabrowski@gmail.com>
2022-09-16 10:22:51 +0200

    Add main activity

---
+++ app/src/main/java/com/przeman/stepper/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.przeman.stepper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            Box {
+
+            }
+        }
+    }
+}
